cfcd3f84deba8af30a87d5196b1e027cb753a8c9
S1lence4ever <102757143+S1lence4ever@users.noreply.github.com>
2023-10-09 09:17:29 -0300

    main.js

--- index/main.js
+++ index/main.js
@@ -1,4 +1,7 @@
+// Pom
 function tocaSomPom () {
   document.querySelector('#som_tecla_pom').play;
 }
-document.querySelectorAll('.tecla');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
